091867b72655d6733989f1441363d2a177cce4f9
AlexHorzhij <97761791+AlexHorzhij@users.noreply.github.com>
2023-05-27 16:23:44 +0300

    add base url

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'https://food-delivery-app-backend.onrender.com';
 
 const instance = axios.create({
   baseURL: BASE_URL,
